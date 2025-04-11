bd3d4b71df63fec257d62b8d6e9543def77578f4
CerebrumCoder <guarddin29@gmail.com>
2025-04-11 19:03:11 +0700

    commit week 1

---
+++ src/UserRepository.java
@@ -0,0 +1,11 @@
+public class UserRepository {
+    private List<User> userList;
+    private List<Seller> sellerList;
+    private List<Buyer> buyerList;
+    private List<Admin> adminList;
+    private List<Sender> senderList;
+
+    public User getUserById(UUID id) {
+        re
+    }
+}
