223d6065ffb37ffd4ce1b1e39f83abddd5c2a361
CerebrumCoder <guarddin29@gmail.com>
2025-04-21 10:08:14 +0700

    new changes

---
+++ src/Penjual.java
@@ -0,0 +1,14 @@
+import user.User;
+
+public class Penjual extends User {
+    private ProductRepository productRepo; // Menyimpan seluruh Product yang dimiliki penjual
+
+    public Penjual(String username, String password) {
+        super(username, password, "Penjual");
+        this.productRepo = new ProductRepository(); // Inisialisasi repository produk untuk penjual
+    }
+
+    public ProductRepository getRepo() {
+        return productRepo;
+    }
+}
