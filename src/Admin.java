4754255ba495180c70ddf174fac2a2aad3a5e578
CerebrumCoder <guarddin29@gmail.com>
2025-04-21 16:50:54 +0700

    tambah fitur baru

---
+++ src/Admin.java
@@ -0,0 +1,17 @@
+public class Admin {
+    private String username; // Username dari admin
+    private String password; // Password dari admin
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/Pengirim.java
@@ -0,0 +1,10 @@
+import user.User;
+
+public class Pengirim extends User {
+
+    public Pengirim(String username, String password) {
+        super(username, password, "Pengirim");
+    }
+
+    // Implementasi tambahan untuk fungsi-fungsi terkait pengiriman dapat ditambahkan di sini
+}
