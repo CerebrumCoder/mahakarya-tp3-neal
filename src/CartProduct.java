24c5bd06964d6dd0c78d804c743d22caa9fbbaf6
CerebrumCoder <guarddin29@gmail.com>
2025-04-21 09:59:45 +0700

    new changes

---
+++ src/CartProduct.java
@@ -0,0 +1,23 @@
+import java.util.UUID;
+
+public class CartProduct {
+    private UUID productId; // Identifier unik untuk produk
+    private int amount; // Jumlah produk yang ingin dibeli
+
+    public CartProduct(UUID productId, int amount) {
+        this.productId = productId;
+        this.amount = amount;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public int getProductAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
--- src/Pembeli.java
+++ src/Pembeli.java
@@ -1,3 +1,14 @@
-public class Pembeli {
-    
+import user.User;
+
+public class Pembeli extends User {
+    private Cart keranjang; // 1-to-1 relationship, satu pembeli memiliki satu cart
+
+    public Pembeli(String username, String password) {
+        super(username, password, "Pembeli");
+        this.keranjang = new Cart(); // Inisialisasi keranjang baru untuk pembeli
+    }
+
+    public Cart getCart() {
+        return keranjang;
+    }
 }
