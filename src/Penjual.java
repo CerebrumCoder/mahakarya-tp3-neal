a31e15f7b02c5aa3a437ef92b5a07c365bed92e0
CerebrumCoder <guarddin29@gmail.com>
2025-04-21 10:11:31 +0700

    new changes

--- src/Penjual.java
+++ src/Penjual.java
@@ -5,7 +5,8 @@ public class Penjual extends User {
 
     public Penjual(String username, String password) {
         super(username, password, "Penjual");
-        this.productRepo = new ProductRepository(); // Inisialisasi repository produk untuk penjual
+        // Ada namaToko yaa
+        this.productRepo = new ProductRepository(""); // Inisialisasi repository produk untuk penjual
     }
 
     public ProductRepository getRepo() {
