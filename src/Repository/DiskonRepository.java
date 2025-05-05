021e99732092c6836ce3ca3a99bbd100fb21a189
CerebrumCoder <guarddin29@gmail.com>
2025-05-05 10:33:45 +0700

    Update di DiskonRepository.java

--- src/Repository/DiskonRepository.java
+++ src/Repository/DiskonRepository.java
@@ -3,7 +3,8 @@ import java.util.List;
 
 // <T> bisa diganti apapun. Tapi tipenya bisa berubah2
 // Ini contoh penggunaan generic yang awalnya Runtime error menjadi Compile error
-// Huruf T atau E itu bebas, pake yang belum di define sebelumnya
+// Huruf T atau E itu bebas, pake yang belum di define sebelumnya.
+// Panggil generic harus pakai tipe class bukan primitive
 public interface DiskonRepository<T> {
     T getById(String id); // Method untuk mendapatkan diskon berdasarkan ID
 
